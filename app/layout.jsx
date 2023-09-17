d4dc8bd081bdef4baff0ad3a17d7b1972e04fdb0
marcushovi <hovimarcus@gmail.com>
2023-09-17 16:56:37 +0200

    upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,31 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/nav';
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
-  description: 'Discover and Share AI Prompts'
-}
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang='en'>
-      <body>
-        <Provider>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
         <div className='main'>
-          <div className='gradient'>
-
-          </div>
+          <div className='gradient' />
         </div>
 
         <main className='app'>
           <Nav />
           {children}
         </main>
-        </Provider>
-      </body>
-    </html>
-  )
-}
+      </Provider>
+    </body>
+  </html>
+);
 
-export default RootLayout
+export default RootLayout;
